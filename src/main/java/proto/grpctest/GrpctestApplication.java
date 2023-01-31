d6c263edb78e0cbca68a308ac5203b81b0cee0c5
lovekite746 <lovekite746@sparrowfasoo.com>
2023-01-31 17:27:28 +0900

    initial commit

---
+++ src/main/java/proto/grpctest/GrpctestApplication.java
@@ -0,0 +1,13 @@
+package proto.grpctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpctestApplication.class, args);
+	}
+
+}
